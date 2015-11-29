858502d1eb88e2b7f7add678945420296f048977
Gius-peppe De Marco <demarcog83@gmail.com>
2015-11-29 23:40:11 +0100

    Update setup_ALL.sh

--- WRF_GFORTRAN/WRF_MPICH2_GENERIC/setup_ALL.sh
+++ WRF_GFORTRAN/WRF_MPICH2_GENERIC/setup_ALL.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 cp WRF-3.6.environment_module /usr/share/modules/modulefiles/WRF-3.6
-module load WRF-3.6
+#module load WRF-3.6
+cp WRF-3.7.environment_module /usr/share/modules/modulefiles/WRF-3.6
+module load WRF-3.7
 
 bash setup_mpich.sh
 bash setup_netcdf.sh
