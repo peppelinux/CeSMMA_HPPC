eafd0277838f35acc4230f3f1be72f0df341a094
Giuseppe De Marco <demarcog83@gmail.com>
2016-12-14 22:06:54 +0100

    Update mars_setup.sh

--- MARS_/mars_setup.sh
+++ MARS_/mars_setup.sh
@@ -14,5 +14,29 @@ pip install virtualenv
 virtualenv $INSTDIR
 cd $INSTDIR
 
+# attivo il virtualenv
+source bin/activate
+
+# installo il client
+pip install https://software.ecmwf.int/wiki/download/attachments/56664858/ecmwf-api-client-python.tgz
+
+# Login into https://apps.ecmwf.int/auth/login/ using the provided username and password.
+# retrieve your key at https://api.ecmwf.int/v1/key/
+
+# install mars script
+mkdir MARS && cd MARS
+wget https://software.ecmwf.int/wiki/download/attachments/56664858/mars
+chmod 755 mars 
+
+# I need to make this setup as system wide, I need to avoid userspace costraints
+# I need to patch lib/python2.7/site-packages/ecmwfapi/api.py
+# line 63
+
+#    rc = os.path.normpath(os.path.expanduser("~/.ecmwfapirc"))
+#to
+#    rc = os.path.normpath("$INSTDIR/MARS/ecmwfapirc")
+#example
+#    rc = os.path.normpath(os.path.expanduser("/opt/MARS.14.12.16/MARS/ecmwfapirc"))
+
 
 
